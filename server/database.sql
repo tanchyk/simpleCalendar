bb7e2dd0922f6a160360fdd3bc4c2653b33e1453
tanchyk <tanchyk.bohdan@gmail.com>
2020-09-17 15:55:33 +0300

    Register and login requests are created

--- server/database.sql
+++ server/database.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE todo_database;
 
-CREATE TABLE todo(
-todo_id SERIAL,
-user_id INTEGER,
-complete BIT DEFAULT 0,
-description VARCHAR(255),
-PRIMARY KEY(todo_id),
-FOREIGN KEY(user_id)
-);
-
 CREATE TABLE users(
 user_id SERIAL,
 email VARCHAR(30) NOT NULL UNIQUE,
-password VARCHAR(20) NOT NULL,
+password VARCHAR(100) NOT NULL,
 PRIMARY KEY(user_id)
+);
+
+CREATE TABLE todo(
+todo_id SERIAL,
+user_id INTEGER,
+complete BOOL DEFAULT false,
+description VARCHAR(255),
+PRIMARY KEY(todo_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,18 +1,96 @@
 import express, {Request, Response, NextFunction} from 'express';
 import {QueryResult} from "pg";
 import {pool} from '../db';
+import {check, validationResult} from 'express-validator';
 
+import config from 'config';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
+interface User {
+    userId: number
+    email: string
+    password: string
+}
 
 const authRouter = express.Router();
 
-authRouter.post('/register', async (req: Request, res: Response) => {
+authRouter.post('/register',
+    [
+        check('email', 'Wrong email').isEmail(),
+        check('password', 'Password should be more than 6 characters').isLength({min: 6})
+    ],
+    async (req: Request, res: Response): Promise<Response> => {
     try {
+        const errors: any = validationResult(req);
 
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Wrong data when trying to sign in'
+            });
+        }
+
+        const {email, password} = req.body;
+        const check: any = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (check.email) {
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        const hashedPassword: string = await bcrypt.hash(password, 4);
+
+        await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+
+        return res.status(201).json({message: 'User created'});
     } catch (e) {
-        res.status(500).json({message: 'Try again'})
+        return res.status(500).json({message: 'Try again'})
     }
-});
+    });
+
+authRouter.post('/login',
+    [
+        check('email', 'Enter correct email').normalizeEmail().isEmail(),
+        check('password', 'Enter password').exists()
+    ],
+    async (req: Request, res: Response): Promise<Response> => {
+        try {
+            const errors: any = validationResult(req);
+
+            if(!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Wrong data when trying to sign in'
+                });
+            }
+
+            const {email, password} = req.body;
+
+            const user: any = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+            if(!user) {
+                return res.status(400).json({message: 'No such user'});
+            }
+
+            const check = await bcrypt.compare(password, user.password);
+
+            if(!check) {
+                return res.status(400).json({message: 'Wrong password'});
+            }
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            return res.status(200).json({
+                token,
+                userId: user.id
+            });
+
+        } catch (e) {
+            return res.status(500).json({message: 'Try again'})
+        }
+    });
 
 export default authRouter;
